63215b5d6fab792c95e53a1ad00412eb3d8d7390
Smile <lijianhy@qq.com>
2015-01-04 22:26:46 +0800

    commit cat

---
+++ src/com/levi/cat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.levi.cat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new PlayGroud(this));
+    }
+}
